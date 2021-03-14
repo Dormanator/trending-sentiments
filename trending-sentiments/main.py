8a66241c4a69ea077843b8e7918b66b3d9bf8b05
Ryan Dorman <ryandormanthe1@gmail.com>
2021-03-13 21:18:06 -0600

    app layout updated, 2 user stats added

--- trending-sentiments/main.py
+++ trending-sentiments/main.py
@@ -7,44 +7,74 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+COLS_TO_SAMPLE = ['id', 'created_at', 'full_text', 'retweet_count', 'favorite_count',
+         'entities.hashtags', 'user.id', 'user.screen_name']
+
+# Connect to Twitter
 auth = tweepy.AppAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_SECRET_KEY'))
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-results = api.search(q='#Avatar', count=100, tweet_mode='extended', result_type='recent')
-json_data = [r._json for r in results]
-
-df = pd.json_normalize(json_data)
-df['created_at'] = pd.to_datetime(df['created_at']).dt.tz_convert(None)
-
-df = df[['id', 'created_at', 'full_text', 'retweet_count', 'favorite_count',
-         'entities.hashtags', 'user.id', 'user.screen_name']]
-
+# Setup Page Title and Header
+st.set_page_config(page_title="Trending Sentiments", page_icon="📈", initial_sidebar_state="expanded",)
 st.write("""
-#  Trending Sentiments
+# Trending Sentiments
 Discover trending sentiments on Twitter with a hashtag or keyword search.
 """)
 
+# Setup Sidebar
+# Handle user input
+search = st.sidebar.text_input('Search Twitter', '#Avatar')
+if not search:
+    st.warning('Please input a search value.')
+    st.stop()
+
+# Search Twitter
+results = api.search(q=search, count=100, tweet_mode='extended', result_type='recent')
+json_data = [r._json for r in results]
+
+# Convert JSON to Dataframe
+df = pd.json_normalize(json_data)
+df['created_at'] = pd.to_datetime(df['created_at']).dt.tz_convert(None)
+df = df[COLS_TO_SAMPLE]
+
+# Start of Twitter result body
+st.write("""
+## 100 Most Recent Tweets for:
+""", search)
+
+# User descriptive statistics
+col1, col2 = st.beta_columns(2)
+user_counts = df['user.screen_name'].value_counts()
+
+# number of unique users kpi
+num_users = user_counts.size
+with col1:
+    st.write("""
+    ### Unique Users
+    """, num_users)
+
+# user with most tweets
+user_max_tweets = user_counts[[0]].index.values[0]
+count_max_tweets = user_counts[[0][0]]
+with col2:
+    st.write("""
+    ### User with Most Tweets
+    """, '@', user_max_tweets, '&nbsp;&nbsp;&nbsp;&nbsp;', count_max_tweets)
+
+
+# Todo: Frequency counts per minute/hour/day of pos & neg tweets
 tweets_per_min = df['created_at'].map(lambda x: x.replace(second=0)).value_counts()
 st.write("""
-## Frequency of 100 Most Recent Tweets
+### Frequency of Tweets
 """)
 st.line_chart(tweets_per_min)
 
 # length of time period 100 most recent occurred in kpi (e.g., Occurred in 6 hours)
-
-# number of unique users kpi
-
 # Current interaction rating: very low (> 24hrs), low (24hrs-12), med (12-4), high (4-2), very high (1)
-
 
 # Scatter of sentiment & intensity over time for tweets
 
-# Frequency counts per minute/hour/day of pos & neg tweets
-
 # top 5 hashtags bar chart
-
-
-# user with most tweets
 
 # favorite tweet & sentiment
 

83e8d2485784b41587584cb0d51de4dc10b3dc27
Ryan Dorman <ryandormanthe1@gmail.com>
2021-03-26 18:45:46 -0500

    added clean_tweet test

--- test/test_transformer_pipeline.py
+++ test/test_transformer_pipeline.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+import numpy as np
 
 from app.transformer_pipeline import TransformerPipeline
 
@@ -16,6 +17,14 @@ class TestTransformerPipeline(unittest.TestCase):
             json_data = json.load(json_file)
             self.test_json = json_data['_json']
 
+    def _mock_sentiment_prediction(self):
+        df_copy = self.test_df.copy()
+        mock_scores = np.arange(-1.0, 1.2, 0.2)
+        mock_text = ['Negative'] * 4 + ['Neutral'] + ['Positive'] * 4
+        df_copy['sentiment_score'] = mock_scores
+        df_copy['sentiment_text'] = mock_text
+        return df_copy
+
     def test_convert_json_to_dataframe(self):
         expected_rows = 10
         expected_cols = ['id', 'created_at', 'full_text', 'tweet', 'retweet_count', 'favorite_count',
@@ -30,10 +39,13 @@ class TestTransformerPipeline(unittest.TestCase):
             self.assertTrue(col in self.test_df)
 
     def test_clean_tweet(self):
-        pass
+        raw_tweet = '  In the #Avatar       https://www.test.com      sequels, you \t' \
+                    '\n won’t just return to Pandora — you’ll explore new parts of the world. @officialavatar '
+        expected_tweet = 'In the sequels, you won’t just return to Pandora — you’ll explore ' \
+                         'new parts of the world.'
 
-    def test_predict_sentiment(self):
-        pass
+        clean_tweet = self.transformer.clean_tweet(raw_tweet)
+        self.assertEqual(clean_tweet, expected_tweet)
 
     def test_map_sentiment_label(self):
         pass

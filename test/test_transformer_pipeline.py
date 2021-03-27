2522e6ef4d45d2c0ea984f7a4add2d4759cbb4df
dormanator <ryandormanthe1@gmail.com>
2021-03-27 12:47:02 -0500

    unit testing added

--- test/test_transformer_pipeline.py
+++ test/test_transformer_pipeline.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 import numpy as np
+import pandas as pd
 
 from app.transformer_pipeline import TransformerPipeline
 
@@ -57,17 +58,63 @@ class TestTransformerPipeline(unittest.TestCase):
         self.assertListEqual(sentiment_text, expected_text)
 
     def test_map_interaction_label(self):
-        mock_time_deltas = []
-        expected_text = []
+        mock_time_deltas = [pd.Timedelta("1 hour"), pd.Timedelta("3 hour"), pd.Timedelta("6 hour"),
+                            pd.Timedelta("13 hour"), pd.Timedelta("26 hour")]
+        expected_text = ['Very High', 'High', 'Medium', 'Low', 'Very Low']
+
+        interaction_text = list(map(self.transformer.map_interaction_label, mock_time_deltas))
+
+        self.assertListEqual(interaction_text, expected_text)
 
     def test_gen_tweets_by_time_dataframe(self):
-        pass
+        mock_df = self.transformer.convert_json_to_dataframe(_load_mock_json())
+        mock_df = _get_mock_sentiment_predictions(mock_df)
+
+        # Tallied manually from ./resources/test_twitter_response.json
+        expected_times = ['3-26-21 14:23:00+00:00', '3-26-21 14:24:00+00:00', '3-26-21 14:26:00+00:00',
+                          '3-26-21 14:27:00+00:00', '3-26-21 14:29:00+00:00', '3-26-21 14:32:00+00:00',
+                          '3-26-21 14:33:00+00:00']
+        expected_counts = [2, 1, 1, 2, 1, 1, 2]
+        expected_df = pd.DataFrame(data={
+            'Created': expected_times,
+            'Tweets': expected_counts
+        })
+        expected_df['Created'] = pd.to_datetime(expected_df['Created'])
+
+        test_df = self.transformer.gen_tweets_by_time_dataframe(mock_df)
+
+        self.assertTrue(test_df.equals(expected_df))
 
     def test_gen_sentiment_score_by_time_dataframe(self):
-        pass
+        mock_cols = ['created_at', 'sentiment_score', 'sentiment_text']
+        mock_df = self.transformer.convert_json_to_dataframe(_load_mock_json())
+        mock_df = _get_mock_sentiment_predictions(mock_df)
+
+        expected_cols = ['Created', 'Sentiment Score', 'Sentiment']
+
+        test_df = self.transformer.gen_sentiment_score_by_time_dataframe(mock_df)
+
+        self.assertListEqual(list(test_df), expected_cols)
+        for i, col in enumerate(expected_cols):
+            self.assertListEqual(test_df[col].values.tolist(), mock_df[mock_cols[i]].values.tolist())
 
     def test_gen_hashtag_counts_dataframe(self):
-        pass
+        mock_df = self.transformer.convert_json_to_dataframe(_load_mock_json())
+        mock_df = _get_mock_sentiment_predictions(mock_df)
+
+        # Tallied manually from ./resources/test_twitter_response.json
+        expected_hashtags = ['trm', 'richracer', 'psn', 'avatar', 'ps3']
+        expected_counts = [9, 9, 2, 1, 1]
+        expected_df = pd.DataFrame(data={
+            'Hashtag': expected_hashtags,
+            'Count': expected_counts
+        })
+
+        test_df = self.transformer.gen_hashtag_counts_dataframe(mock_df)
+        test_top_5 = test_df.loc[:4]
+
+        self.assertListEqual(test_top_5['Hashtag'].to_list(), expected_df['Hashtag'].to_list())
+        self.assertListEqual(test_top_5['Count'].to_list(), expected_df['Count'].to_list())
 
 
 if __name__ == '__main__':
